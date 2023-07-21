6bf8a0f4c6c864336b8b8c0417c31eaf55b480fc
iitianshivam <138360022+iitianshivam@users.noreply.github.com>
2023-07-22 03:09:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+const quizData = [
+    {
+        question: "Which AI program, developed by IBM, defeated the world chess champion Garry Kasparov in 1997?",
+        a: "Stockfish",
+        b: "Siri",
+        c: "Leela Chess Zero",
+        d: "Deep Blue",
+        correct: "d",
+    },
+    {
+        question: "Which practice focuses on physical, mental, and spiritual practices to achieve harmony and balance in life?",
+        a: "Meditation",
+        b: "Yoga",
+        c: "Vedanta",
+        d: "Buddhism",
+        correct: "b",
+    },
+    {
+        question: "Who painted the famous artwork Mona Lisa?",
+        a: "Leonardo da Vinci",
+        b: "Pablo Picasso",
+        c: "Vincent van Gogh",
+        d: "Michelangelo",
+        correct: "a",
+    },
+    {
+        question: "Which famous scientist formulated the theory of relativity, E=mc²?",
+        a: "Isaac Newton",
+        b: "Albert Einstein",
+        c: "Srinivasa Ramanujan",
+        d: "Galileo Galilei",
+        correct: "b",
+    },
+];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
